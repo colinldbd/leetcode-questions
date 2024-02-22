00df2f7e9e3d708be38cb1ebf7cc548bcec30c5e
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-02-23 03:43:11 +1100

    Added  IndexofTheFirstOccurenceInString

---
+++ IndexofTheFirstOccurenceInString/IndexofTheFirstOccurenceInString.cs
@@ -0,0 +1,50 @@
+﻿namespace IndexofTheFirstOccurenceInString
+{
+    internal class IndexofTheFirstOccurenceInString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack.Contains(needle))
+            {
+                string[] result = haystack.Split(needle, StringSplitOptions.None);
+
+                return result[0].Length;
+            }
+
+            return -1;
+        }
+
+        public int StrSt2(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+
+        // Or try to loop through the occurrence of needle in haystack
+        //public int StrSt2(string haystack, string needle)
+        //{
+        //    return haystack.IndexOf(needle);
+        //}
+
+        // Or by using substring
+        public int StrStr3(string haystack, string needle)
+        {
+            if (needle.Length > haystack.Length)
+            {
+                return -1;
+            }
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack.Substring(i, i + needle.Length).Equals(needle))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
