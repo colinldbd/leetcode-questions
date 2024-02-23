e952f0518ba66fe0dd0ca3be878afd2072226598
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-02-24 04:06:18 +1100

    Added leetcode 392.

---
+++ IsSubsequence/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IsSubsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public bool IsSubsequence(string s, string t)
+        {
+            string result = "";
+
+            if (t.Length < s.Length) return false;
+
+            for (int i = 0, j = 0; j < t.Length && i < s.Length; j++)
+            {
+                if (t[j] == s[i])
+                {
+                    result += t[j];
+                    i++;
+                }
+            }
+
+            if (result == s)
+                return true;
+            else
+                return false;
+        }
+    }
+}
