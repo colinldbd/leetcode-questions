d86ffd659cb2ebd2da39c50673bdb1a9bba36916
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-02-26 01:50:03 +1100

    Hashmap questions.

---
+++ ValidAnagram/ValidAnagram.cs
@@ -0,0 +1,47 @@
+﻿namespace ValidAnagram
+{
+    internal class ValidAnagram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public bool IsAnagram(string s, string t)
+        {
+            Dictionary<char, int> d1 = new Dictionary<char, int>();
+            Dictionary<char, int> d2 = new Dictionary<char, int>();
+
+            if (s.Length != t.Length) return false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!d1.ContainsKey(s[i]))
+                {
+                    d1[s[i]] = 0;
+                }
+                if (!d2.ContainsKey(t[i]))
+                {
+                    d2[t[i]] = 0;
+                }
+                d1[s[i]]++;
+                d2[t[i]]++;
+            }
+
+            if(d1.Count() != d2.Count()) { return false; }
+            foreach(char c in s)
+            {
+                if (!d2.Keys.Contains(c))
+                {
+                    return false;
+                }
+                if (d1[c] != d2[c])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ WordPattern/WordPattern.cs
@@ -0,0 +1,40 @@
+﻿namespace WordPattern
+{
+    internal class WordPattern
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            SolveWordPattern("abba", "dog cat cat dog");
+        }
+
+        public static bool SolveWordPattern(string pattern, string s)
+        {
+            string[] sArray = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<char, int> pDict = new Dictionary<char, int>();
+            Dictionary<string, int> sDict = new Dictionary<string, int>();
+
+            if (pattern.Length != sArray.Length) return false;
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (!pDict.ContainsKey(pattern[i]))
+                {
+                    pDict[pattern[i]] = i;
+                }
+                if (!sDict.ContainsKey(sArray[i]))
+                {
+                    sDict[sArray[i]] = i;
+                }
+
+                if (pDict[pattern[i]] != sDict[sArray[i]])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
