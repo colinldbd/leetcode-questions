9f5e9d273cf9788bc56406841c7c0da82067960d
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-02-26 21:57:54 +1100

    Added two sum.

---
+++ TwoSum/TwoSum.cs
@@ -0,0 +1,33 @@
+﻿namespace TwoSum
+{
+    internal class TwoSum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int[] returnTwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int[] result = new int[2];
+
+            for(int i  = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+
+                if (dict.ContainsKey(diff))
+                {
+                    result = [dict[diff], i];
+                    break;
+                }
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]] = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
