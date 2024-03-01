b9719799bc7f39757e1ef4ff1831484c78a19cde
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-03-01 18:16:15 +1100

    Added linked list

---
+++ LinkedListCycle/LinkedListCycle.cs
@@ -0,0 +1,55 @@
+﻿namespace LinkedListCycle
+{
+    internal class LinkedListCycle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        /**
+        * Definition for singly-linked list.
+        * public class ListNode {
+        *     public int val;
+        *     public ListNode next;
+        *     public ListNode(int x) {
+        *         val = x;
+        *         next = null;
+        *     }
+        * }
+        */
+        public bool HasCycle(ListNode head)
+        {
+            HashSet<ListNode> visited = new HashSet<ListNode>();
+
+            if(head == null)
+            {
+                return false;
+            }
+
+            while(head.next != null)
+            {
+                if (visited.Contains(head.next))
+                {
+                    return true;
+                }
+
+                visited.Add(head);
+                head = head.next;
+            }
+
+            return false;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode? next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+    }
+}
---
+++ MergeTwoSortedLists/MergeTwoSortedLists.cs
@@ -0,0 +1,66 @@
+﻿namespace MergeTwoSortedLists
+{
+    internal class MergeTwoSortedLists
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        /**
+         * Definition for singly-linked list.
+         * public class ListNode {
+         *     public int val;
+         *     public ListNode next;
+         *     public ListNode(int val=0, ListNode next=null) {
+         *         this.val = val;
+         *         this.next = next;
+         *     }
+         * }
+         */
+
+        public ListNode MergeLists(ListNode list1, ListNode list2)
+        {
+            ListNode? head = new ListNode();
+            ListNode? tail = null;
+
+            tail = head;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list2.val <= list1.val)
+                {
+                    tail.next = list2;
+                    list2 = list2.next;
+                }
+                else
+                {
+                    tail.next = list1;
+                    list1 = list1.next;
+                }
+
+                tail = tail.next;
+            }
+
+            if(list1 != null){
+                tail.next = list1;
+            }
+            if(list2 != null)
+            {
+                tail.next = list2;
+            }
+
+            return head.next;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
