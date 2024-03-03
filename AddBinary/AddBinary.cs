751f3a65323470bc7c0614699b187f29acdd1ca0
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-03-03 19:05:23 +1100

    Bit operation

---
+++ AddBinary/AddBinary.cs
@@ -0,0 +1,83 @@
+﻿namespace AddBinary
+{
+    internal class AddBinary
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            AddBinaryString2("1010", "1011");
+        }
+
+
+        //Can't convert, will pass the limit for integer.
+        public static string AddBinaryString(string a, string b)
+        {
+            long sum = ToDeximal(a) + ToDeximal(b);
+            string binary = string.Empty;
+            string reverse = string.Empty;
+
+            do
+            {
+                binary += (sum % 2).ToString();
+                sum /= 2;
+            } while (sum != 0);
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                reverse += binary[binary.Length - i - 1];
+            }
+
+            return reverse;
+        }
+
+        public static long ToDeximal(string binary)
+        {
+            if (binary == null || binary == String.Empty)
+            {
+                return 0;
+            }
+            long num = 0;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                num += (long)Char.GetNumericValue(binary[binary.Length - i - 1]) * (long)Math.Pow(2, i);
+
+            }
+
+            return num;
+        }
+
+        public static string? AddBinaryString2(string a, string b)
+        {
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            int carry = 0;
+
+            string result = string.Empty;
+
+            while (i >= 0 || j >= 0)
+            {
+                int sum = 0;
+                sum += carry;
+                //int sum = carry;
+                // Don't write as (int)a[i--], it will reture the ASCII number
+                if (i >= 0)
+                    sum += (int)Char.GetNumericValue(a[i--]);
+                if (j >= 0)
+                    sum += (int)Char.GetNumericValue(b[j--]);
+                carry = sum > 1 ? 1 : 0;
+                result += (sum % 2);
+            }
+
+            if (carry != 0) { result += carry; }
+
+            char[] cArray = result.ToCharArray();
+            Array.Reverse(cArray);
+
+            result = new string(cArray);
+
+            return result;
+        }
+    }
+}
---
+++ SearchInsertPosition/SearchInsertPosition.cs
@@ -0,0 +1,67 @@
+﻿namespace SearchInsertPosition
+{
+    internal class SearchInsertPosition
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int result = SearchInsert([1, 3, 5, 6], 2);
+
+            return;
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            return BinarySearch(nums, 0, nums.Length - 1, target);
+        }
+
+        public static int BinarySearch(int[] nums, int l, int r, int target)
+        {
+            // Why just return l? Because if target can't be found, there're two types of situations
+            // 1. r is subtracted to less than l, in this case, we need to insert it to the left side of l
+            // thus l
+            // 2. l is added to more than r, in this case, we need to insert it to the right side of r.
+            // but because l is larger than r(by 1), so l = r + 1,
+            // thus also l.
+            if (l > r)
+            {
+                return l;
+            }
+
+            int mid = (l + r) / 2;
+
+            //Left closed right open interval to better represent the index, because we're using l+1
+            if (target < nums[mid])
+            {
+                r = mid - 1;
+            }
+            else if (target > nums[mid])
+            {
+                l = mid + 1;
+            }
+            else { return mid; }
+
+            return BinarySearch(nums, l, r, target);
+        }
+
+        int BinarySearch2(int[] nums, int target)
+        {
+            // 初始化双闭区间 [0, n-1] ，即 i, j 分别指向数组首元素、尾元素
+            int i = 0, j = nums.Length - 1;
+            // 循环，当搜索区间为空时跳出（当 i > j 时为空）
+            while (i <= j) //注意这里是i<=j 相当于 if i > j ,break
+            {
+                int m = i + (j - i) / 2;   // 计算中点索引 m
+                if (nums[m] < target)      // 此情况说明 target 在区间 [m+1, j] 中
+                    i = m + 1;
+                else if (nums[m] > target) // 此情况说明 target 在区间 [i, m-1] 中
+                    j = m - 1;
+                else                       // 找到目标元素，返回其索引
+                    return m;
+            }
+            // 未找到目标元素，返回 -1
+            return i;
+        }
+    }
+}
