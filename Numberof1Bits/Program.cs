2a632a5726f14fa182e5c13b2567c91834e952a8
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-03-03 23:19:14 +1100

    Added count number of 1 in uint.

---
+++ Numberof1Bits/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Numberof1Bits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                if ((n & 1) == 1) count++;
+                n >>= 1;
+            }
+
+            return count;
+
+            //var str = Convert.ToString(n, 2);
+            //var result = 0;
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    if (str[i] == '1') { result += 1; }
+            //}
+            //return result;
+        }
+    }
+}
