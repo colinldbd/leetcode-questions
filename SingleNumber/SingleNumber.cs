998d78ebffdd14e2adf1dfa54a68b83eb52b9432
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-03-04 11:58:24 +1100

    Added single number detection by using XOR.

---
+++ SingleNumber/SingleNumber.cs
@@ -0,0 +1,20 @@
+﻿namespace SingleNumber
+{
+    internal class SingleNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public int GetSingleNumber(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                nums[i + 1] ^= nums[i];
+            }
+
+            return nums[^1];
+        }
+    }
+}
