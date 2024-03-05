9147f60aac6ad6f589288588c5d7a872f96fdcc7
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-03-06 01:13:03 +1100

    Start of Medium difficult questions.

---
+++ RemoveDuplicatesfromSortedArrayII/RemoveDuplicatesfromSortedArrayII.cs
@@ -0,0 +1,49 @@
+﻿namespace RemoveDuplicatesfromSortedArrayII
+{
+    internal class RemoveDuplicatesfromSortedArrayII
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            RemoveDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]);
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int k = 0;
+            int count = 0;
+            // Remember to deal with edge case,
+            k++;
+            count++;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == nums[i - 1] && count < 2)
+                {
+                    nums[k++] = nums[i];
+                    count++;
+                }
+                else if (nums[i] != nums[i - 1])
+                {
+                    nums[k++] = nums[i];
+                    count = 1; // Count is always 1 initially.
+                }                
+            }
+
+            //int j = 1;
+            //for (int i = 1; i < nums.Length; i++)
+            //{
+            //    if (j == 1 || nums[i] != nums[j - 2])
+            //    {
+            //        nums[j++] = nums[i];
+            //    }
+            //}
+            //return j;
+
+            return k;
+        }
+    }
+}
---
+++ RotateArray/RotateArray.cs
@@ -0,0 +1,37 @@
+﻿namespace RotateArray
+{
+    internal class RotateArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Rotate([1, 2, 3, 4, 5, 6, 7], 3);
+        }
+
+        public static void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            //if (k < 0) k += nums.Length;
+
+            Reverse(nums, 0, nums.Length - k - 1);
+            Reverse(nums, nums.Length - k, nums.Length - 1);
+            Reverse(nums, 0, nums.Length - 1);
+        }
+
+        public static void Reverse(int[] nums, int i, int j)
+        {
+            int li = i;
+            int ri = j;
+
+            while (li < ri)
+            {
+                int temp = nums[li];
+                nums[li] = nums[ri];
+                nums[ri] = temp;
+
+                li++;
+                ri--;
+            }
+        }
+    }
+}
