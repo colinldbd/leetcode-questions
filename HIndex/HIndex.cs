2cf1f34d88802c4e14d23270a0ceca35d05cbffe
Colin Xuesi Peng <u7446077@anu.edu.au>
2024-03-09 15:36:44 +1100

    HIndex and InsertDeleteGetRandom

---
+++ HIndex/HIndex.cs
@@ -0,0 +1,66 @@
+﻿namespace HIndex
+{
+    internal class HIndex
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            GetHIndex([11, 15]);
+        }
+
+        public static int GetHIndex(int[] citations)
+        {
+            citations = citations.Order().ToArray();
+
+            int h = 0;
+            for (int i = 0; i < citations.Length; i++)
+            {
+                if (citations.Length - 1 - i >= citations[i] - 1)
+                {
+                    if (h < citations[i])
+                        h = citations[i];
+                }
+                else
+                {
+                    if (h < citations.Length - i)
+                    {
+                        h = citations.Length - i;
+                    }
+                }
+            }
+
+            return h;
+        }
+
+        // Utilzing bucket sort.
+        public static int GetHIndex2(int[] citations)
+        {
+            int[] buckets = new int[citations.Length + 1];
+
+            foreach (int c in citations)
+            {
+                if (c >= citations.Length)
+                {
+                    buckets[citations.Length]++;
+                }
+                else
+                {
+                    buckets[c]++;
+                }
+            }
+
+            int count = 0;
+            for (int i = citations.Length; i >= 0; i--)
+            {
+                count += buckets[i];
+                if (i <= count)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+
+    }
+}
---
+++ InsertDeleteGetRandomO(1)/RandomeizedSet.cs
@@ -0,0 +1,53 @@
+﻿namespace InsertDeleteGetRandomO_1_
+{
+    internal class RandomeizedSet
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        List<int> set = new List<int>();
+
+        public RandomeizedSet()
+        {
+
+        }
+
+        public bool Insert(int val)
+        {
+            if (!set.Contains(val))
+            {
+                set.Add(val);
+                return true;
+            }
+            else return false;
+        }
+
+        public bool Remove(int val)
+        {
+            if (set.Contains(val))
+            {
+                set.Remove(val);
+                return true;
+            }
+            else return false;
+        }
+
+        public int GetRandom()
+        {
+            Random random = new Random();
+            int index = random.Next()%set.Count;
+
+            return set[index];
+        }
+    }
+    /**
+     * Your RandomizedSet object will be instantiated and called as such:
+     * RandomizedSet obj = new RandomizedSet();
+     * bool param_1 = obj.Insert(val);
+     * bool param_2 = obj.Remove(val);
+     * int param_3 = obj.GetRandom();
+     */
+}
+
