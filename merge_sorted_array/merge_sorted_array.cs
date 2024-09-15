c7a2c9a9bced6e24e549ecfcea7c73963493289d
Colin Xuesi Peng <colinxspeng@outlook.com>
2024-09-15 13:17:21 +1000

    update

--- merge_sorted_array/merge_sorted_array.cs
+++ merge_sorted_array/merge_sorted_array.cs
@@ -9,63 +9,89 @@
             int[] nums2 = [1];
 
             Merge(nums1, 0, nums2, 1);
-        }
-
-        public static void Merge(int[] nums1, int m, int[] nums2, int n)
-        {
-            List<int> list = new();
-
-            nums1 = IgnoreZero(nums1);
-            nums2 = IgnoreZero(nums2);
-
-            for (int i = 0, j = 0; i < m || j < n;)
-            {
-                if (i == m)
-                {
-                    list.Add(nums2[j]);
-                    j++;
-                    continue;
-                }
-                else if (j == n)
-                {
-                    list.Add(nums1[i]);
-                    i++;
-                    continue;
-                }
-
-
-                if (nums1[i] <= nums2[j])
-                {
-                    list.Add(nums1[i]);
-                    i++;
-                }
-                else
-                {
-                    list.Add(nums2[j]);
-                    j++;
-                }
-            }
-
-            nums1 = list.ToArray();
 
             return;
         }
 
-        public static int[] IgnoreZero(int[] nums)
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
         {
-            List<int> list = [];
+            int i = m - 1;
+            int j = n - 1;
+            int k = m + n - 1;
 
-            foreach (int n in nums)
+            // Think it as if you are trying to insert all the numbers from nums2 into nums1, if all j is consumed by nums1, then all numbers in nums2 has found its place.
+            while (j >= 0)
             {
-                if (n != 0)
+                if (i >= 0 && nums2[j] < nums1[i])
                 {
-                    list.Add(n);
+                    nums1[k] = nums1[i];
+                    i--;
                 }
+                else
+                {
+                    nums1[k] = nums2[j];
+                    j--;
+                }
+
+                k--;
             }
-
-            nums = list.ToArray();
-
-            return nums;
         }
+
+        //public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        //{
+        //    List<int> list = new();
+
+        //    nums1 = IgnoreZero(nums1);
+        //    nums2 = IgnoreZero(nums2);
+
+        //    for (int i = 0, j = 0; i < m || j < n;)
+        //    {
+        //        if (i == m)
+        //        {
+        //            list.Add(nums2[j]);
+        //            j++;
+        //            continue;
+        //        }
+        //        else if (j == n)
+        //        {
+        //            list.Add(nums1[i]);
+        //            i++;
+        //            continue;
+        //        }
+
+
+        //        if (nums1[i] <= nums2[j])
+        //        {
+        //            list.Add(nums1[i]);
+        //            i++;
+        //        }
+        //        else
+        //        {
+        //            list.Add(nums2[j]);
+        //            j++;
+        //        }
+        //    }
+
+        //    nums1 = list.ToArray();
+
+        //    return;
+        //}
+
+        //public static int[] IgnoreZero(int[] nums)
+        //{
+        //    List<int> list = [];
+
+        //    foreach (int n in nums)
+        //    {
+        //        if (n != 0)
+        //        {
+        //            list.Add(n);
+        //        }
+        //    }
+
+        //    nums = list.ToArray();
+
+        //    return nums;
+        //}
     }
 }
